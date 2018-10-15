ee887ded8bec6bf8ba479908a806c9756e78fd1d
mark heath <mjpeg0@silicontrip.org>
2018-10-16 09:08:55 +1100

    2d Board location

---
+++ BoardLocation.java
@@ -0,0 +1,30 @@
+public class BoardLocation
+	private int x;
+	private int y;
+
+	public BoardLocation(int xx, int yy) {
+		x = xx;
+		y = yy;
+	}
+
+	public Boolean isOrigin () { return x==0 && y==0; }
+	public BoardLocation getWest { return new BoardLocation (x-1,y); }
+	public BoardLocation getEast { return new BoardLocation (x+1,y); }
+	public BoardLocation getNorth { return new BoardLocation (x,y-1); }
+	public BoardLocation getSouth { return new BoardLocation (x,y+1); }
+		
+
+        @Override
+        public boolean equals (Object o)
+        {
+                if (o == this) return true;
+                if (!(o instanceof BoardLocation)) return false;
+                BoardLocation l = (BoardLocation) o;
+		return l.x==x && l.y==y;
+        }
+
+	@Override
+	public int hasCode()
+	{
+		return x << 16 ^ y;
+	}
