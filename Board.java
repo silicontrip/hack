2d4b6841c684edb93f7f4d6a14a3cbd9d53c9612
mark heath <mjpeg0@silicontrip.org>
2018-10-16 00:51:44 +1100

    Play, toString methods. fixed first card valid logic.

--- Board.java
+++ Board.java
@@ -13,7 +13,7 @@ public class Board {
 // much game logic here
 	public Boolean isValid(int x, int y, Card c) {
 		// If the board is empty and a start card is played
-		if (bd.size() == 0)
+		if (bd.size() == 0 && x==0 && y==0)
 			return c.isStart();
 
 		// if placing a card on another card
@@ -32,10 +32,20 @@ public class Board {
 		
 	}
 
+	public void play(int x, int y, Card c) {
+
+			if (isValid(x,y,c)) {
+				bd.set(x,y,c);
+			}
+	}
+
 	public Card getNorth(int x, int y) { return bd.get(x,y-1); }
 	public Card getEast(int x, int y) { return bd.get(x+1,y); }
 	public Card getSouth(int x, int y) { return bd.get(x,y+1); }
 	public Card getWest(int x, int y) { return bd.get(x-1,y); }
 
+	public String toString() {
+		return bd.toString();
+	}
 
 }
