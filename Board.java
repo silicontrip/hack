041dfc17fdfe6d71f181fd8979fea2c43e89d3b8
Mark Heath <silicontrip@gmail.com>
2018-10-16 11:25:52 +1100

    Removing X,Y references to Board. Will use BoardLocation from here.

--- Board.java
+++ Board.java
@@ -10,11 +10,11 @@ public class Board {
 		board = new HashBoard();
 	}
 
-	public Boolean isEmpty (int x, int y) { return board.isEmpty(x,y); } 
+	//public Boolean isEmpty (int x, int y) { return board.isEmpty(x,y); } 
 	public Boolean isEmpty (BoardLocation b) { return board.isEmpty(b); } 
 
 	// much game logic here
-	public Boolean isValid(int x, int y, Card c) { return isValid(new BoardLocation(x,y),c);  }
+//	public Boolean isValid(int x, int y, Card c) { return isValid(new BoardLocation(x,y),c);  }
 	public Boolean isValid(BoardLocation bl, Card c) {
 		// If the board is empty and a start card is played
 		if (board.size() == 0 && bl.isOrigin())
@@ -25,7 +25,7 @@ public class Board {
 			return false;
 		
 		// if placing a card next to no other card
-		if ( getNorthCard(bl) == null && getEastCard(bl) == null && getSouthCard(bl) == null && getWestCard(bl) == null )
+		if ( isEmpty(bl.getNorth()) && isEmpty(bl.getEast()) && isEmpty(bl.getSouth()) && isEmpty(bl.getWest())) 
 			return false;
 
 		// if all 4 compass points match (matching against an empty square is valid)
@@ -36,40 +36,41 @@ public class Board {
 		
 	}
 
-	public void play(int x, int y, Card c) {
-			if (isValid(x,y,c)) {
-				board.set(x,y,c);
+	//public void play(int x, int y, Card c) {
+	public void play(BoardLocation bl, Card c) {
+			if (isValid(bl,c)) {
+				board.set(bl,c);
 			}
 	}
 
-	public HashSet<BoardLocation> getEmptyAdjacent(BoardLocation b) 
+	public HashSet<BoardLocation> getEmptyAdjacent(BoardLocation bl) 
 	{
 		HashSet<BoardLocation> empty = new HashSet<BoardLocation>();
-		if (isEmpty(b.getNorth())) empty.add(b.getNorth());
-		if (isEmpty(b.getEast())) empty.add(b.getEast());
-		if (isEmpty(b.getSouth())) empty.add(b.getSouth());
-		if (isEmpty(b.getWest())) empty.add(b.getWest());
+		if (isEmpty(bl.getNorth())) empty.add(bl.getNorth());
+		if (isEmpty(bl.getEast())) empty.add(bl.getEast());
+		if (isEmpty(bl.getSouth())) empty.add(bl.getSouth());
+		if (isEmpty(bl.getWest())) empty.add(bl.getWest());
 		return empty;
 	}
 
 	public HashSet<BoardLocation> getValidSpace()
 	{
 		HashSet<BoardLocation> validSpaces = new HashSet<BoardLocation>();
-		for (BoardLocation b : board.getUsedLocations())
+		for (BoardLocation bl : board.getUsedLocations())
 		{
 			// add all empty adjacent spaces
-			validSpaces.addAll(getEmptyAdjacent(b));
+			validSpaces.addAll(getEmptyAdjacent(bl));
 		}
 		return validSpaces;
 	}
 
-	public Card getNorthCard(int x, int y) { return board.get(x,y-1); }
+	//public Card getNorthCard(int x, int y) { return board.get(x,y-1); }
 	public Card getNorthCard(BoardLocation bl) { return board.get(bl.getNorth()); }
-	public Card getEastCard(int x, int y) { return board.get(x+1,y); }
+	//public Card getEastCard(int x, int y) { return board.get(x+1,y); }
 	public Card getEastCard(BoardLocation bl) { return board.get(bl.getEast()); }
-	public Card getSouthCard(int x, int y) { return board.get(x,y+1); }
+	//public Card getSouthCard(int x, int y) { return board.get(x,y+1); }
 	public Card getSouthCard(BoardLocation bl) { return board.get(bl.getSouth()); }
-	public Card getWestCard(int x, int y) { return board.get(x-1,y); }
+	//public Card getWestCard(int x, int y) { return board.get(x-1,y); }
 	public Card getWestCard(BoardLocation bl) { return board.get(bl.getWest()); }
 
 	public String toString() {
