aa01cb1b062bd160487ef8e2389eea6ce90a08b2
mark heath <mjpeg0@silicontrip.org>
2018-10-17 16:41:31 +1100

    Starting on UI

---
+++ Hack.java
@@ -0,0 +1,33 @@
+public class Hack {
+
+// determine how many players
+// determine player object types
+
+// player ArrayList
+// show deck
+// show board
+
+// determine first player
+// rotate array to first player
+
+// scores
+// winner
+// deck size
+
+// while (alldecks>0 && !winner)
+//{
+//for (Player : PlayerArray)
+// {
+// Player.requestMove()
+// isValid
+// board.play(card)
+// other Players: update board
+// winner ? allPlaeyrs show winner
+// Player.deck.removeCard(card)
+// calculate Scores
+// show Scores
+// }
+//}
+
+
+}
