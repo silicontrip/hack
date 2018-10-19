eff152d90ef620d46cda2ad35b16f8621d14f422
mark heath <mjpeg0@silicontrip.org>
2018-10-19 19:12:20 +1100

    shell script to rotate and rename the card Images.

---
+++ Cards/rotate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for n in $@
+do
+	for i in 1 2 3 4
+	do
+		nn=$(echo "${n:0:1}${n:2:1}${n:3:1}${n:4:1}${n:1:1}${n:5:4}")
+		if [ !  -e "$nn" ]
+		then
+			echo "rotate... $n -> $nn"
+			convert -rotate 90  "$n"  "$nn"
+		fi
+		n=$nn
+	done	
+done
